883d581a9d31ce0813d36dff0716874fdce3dd3c
Åsa Persson <asa.persson@isotop.se>
2020-11-10 14:31:20 +0100

    format date in daydetails

--- src/DayDetails.tsx
+++ src/DayDetails.tsx
@@ -7,15 +7,14 @@ interface DayDetailsPropps {
     day:Date;
 }
 const DayDetails: React.FC<DayDetailsPropps> = ({ day }) => {
-    const numberOfDay = format(day, "dd", { locale: svLocale });
-
-return <>
-    <div className="day-datails">
-    <p className="day-number">{numberOfDay}</p>
-
-       hej en dag med alla event å taggar här tack
-    </div>  
-    </>
-  }
+    const dateOfDay = format(day, "dd MMMM yyyy", { locale: svLocale });
+    const nameOfDay = format(day, "EEEE", {locale:svLocale});
+    return <>
+        <div className="day-datails">
+        <p className="day-number">{nameOfDay} {dateOfDay}</p>
+        hej en dag med alla event å taggar här tack
+        </div>  
+        </>
+    }
 
   export default DayDetails;
